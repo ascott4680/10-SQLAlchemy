3ca28305ef5d4eb8a1308fa6231bf342362a9354
Andrew Scott <andrewscott@Andrews-MacBook-Pro.local>
2019-05-02 18:35:38 -0700

    Update 4

--- scott_flask.py
+++ scott_flask.py
@@ -41,7 +41,7 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
     )
 
 
@@ -52,7 +52,6 @@ def precipitation():
     results = session.query(Measurement.prcp, Measurement.date).all()
 
     # Convert list of tuples into normal list
-    #prcp = list(np.ravel(results))
     prcp_dict = []
     for result in results:
         prcp = {}
@@ -70,6 +69,40 @@ def stations():
 
     results = list(np.ravel(stations))
     return jsonify(results)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """query for the dates and temperature observations from a year from the last data point.
+Return a JSON list of Temperature Observations (tobs) for the previous year."""
+    # Query
+    tobs_results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >='2016-08-23').all()
+    tobs = list(np.ravel(tobs_results))
+    return jsonify(tobs)
+
+#books = sess.query(Book).filter(Book.title.like(likeString))
+
+
+@app.route("/api/v1.0/<start>")
+def s_start(start):
+    """When given the start only, calculate TMIN, TAVG, and TMAX for all
+     dates greater than and equal to the start date."""
+    start_results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    start1 = list(np.ravel(start_results))
+    return jsonify(start1)
+
+@app.route("/api/v1.0/<start>/<end>")
+def s_start(start,end):
+    """Lolz"""
+    if end != None
+    results_start_end = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    start_end = list(np.ravel(results_start_end))
+    return jsonify(start_end)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
